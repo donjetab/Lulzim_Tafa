684bf87f452c5fc25c11e05cda62ece8e447a976
Donjeta <donjetabajgora13@gmail.com>
2026-06-29 13:06:02 +0200

    Use HashRouter for GitHub Pages 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,8 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import './styles/global.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
