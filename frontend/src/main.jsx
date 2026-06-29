d0b3ccece32e656926882d1951863dc9e85a27e7
Donjeta <donjetabajgora13@gmail.com>
2026-06-29 13:10:07 +0200

    Fix main css import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,7 +17,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
 import './styles/global.css';
 
 
